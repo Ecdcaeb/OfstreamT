80eca3f987dd01cd3ee5c99e495c062c71a6f4f2
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2023-04-30 19:00:14 +0800

    change version
    delete 1.12.2

--- src/main/java/com/Hileb/ofstream/ofstream/IDataOfstream.java
+++
@@ -1,5 +0,0 @@
-package com.Hileb.ofstream.ofstream;
-
-public interface IDataOfstream {
-    default Object returnObj(){return this;}
-}
--- src/main/java/com/Hileb/ofstream/proxy/ServerProxy.java
+++
@@ -1,9 +0,0 @@
-package com.Hileb.ofstream.proxy;
-
-public class ServerProxy extends ProxyBase {
-    public static final String PROXY="com.Hileb.ofstream.proxy.ServerProxy";
-    public boolean isServer()
-    {
-        return true;
-    }
-}
